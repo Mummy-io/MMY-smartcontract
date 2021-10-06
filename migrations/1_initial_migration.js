f3d75b2332a6cfdaebf46883c6fa95cf762d69c2
nicetech-creator <toptech20190609@gmail.com>
2021-10-07 01:51:57 +0800

    update admin wallet address in migration script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 const MMY = artifacts.require("MMY");
 
-const ADMIN_ADDRESS = "0x5Ba587a9927d5fAC115CC0154045B29CB4Df2839";
+const ADMIN_ADDRESS = "0x817171DA3f7Db4CF909FBB40876852cD5e02e6Bb";
 
 module.exports = async function (deployer) {
   await deployer.deploy(MMY, ADMIN_ADDRESS);
