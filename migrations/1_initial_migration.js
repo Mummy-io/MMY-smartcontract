9b72604f1e74f4967d3313e579142a0469447d91
MummyLego <89541269+MummyLego@users.noreply.github.com>
2024-07-25 08:54:22 +0300

    change ADMIN_ADRESS to a multi owner address

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 const MMY = artifacts.require("MMY");
 
-const ADMIN_ADDRESS = "0x817171DA3f7Db4CF909FBB40876852cD5e02e6Bb";
+const ADMIN_ADDRESS = "0xece6525911960c9E70d33BA7A152ddBa850148B8";
 
 module.exports = async function (deployer) {
   await deployer.deploy(MMY, ADMIN_ADDRESS);
